b37296e96c86571d7106ee9d6d46751404e1873c
Nicolas Gandriau <Nicolas.Gandriau@dealer.com>
2015-03-26 11:30:43 -0400

    - added rest calls -- wacky switch On or Off -- wacky switch status
    - chrome DHC extension lib

--- src/main/groovy/switcher/WackyController.groovy
+++ src/main/groovy/switcher/WackyController.groovy
@@ -9,16 +9,36 @@ import javax.validation.Valid
 
 import static org.springframework.http.HttpStatus.NON_AUTHORITATIVE_INFORMATION
 import static org.springframework.web.bind.annotation.RequestMethod.POST
+import static org.springframework.web.bind.annotation.RequestMethod.GET
 
 @RestController
 @RequestMapping("/wacky")
 public class WackyController {
 
 
-    @RequestMapping(method = POST)
+    @RequestMapping(value="/switchOnFor", method = POST)
     @ResponseStatus(NON_AUTHORITATIVE_INFORMATION)
-    public void witch( @Valid @RequestParam long time) {
+    public void switchOnFor( @Valid @RequestParam long time) {
 
-        println "POST::wacky?time=$time"
+        println "POST::switchFor?time=$time"
     }
+
+    @RequestMapping(value="/switch", method = POST)
+    @ResponseStatus(NON_AUTHORITATIVE_INFORMATION)
+    public void switchOnOrOff( @Valid @RequestParam boolean on) {
+
+        println "POST::switchOnorOff?on=$on"
+    }
+
+    @RequestMapping(value="/switch", method = GET)
+    public boolean switchStatus( ) {
+
+        println "GET::switchStatus"
+
+        false
+    }
+
+
+
+
 }
