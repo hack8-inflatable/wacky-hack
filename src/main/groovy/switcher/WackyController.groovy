74249244807c8f13cfa6de6119297cb16d817b77
Nicolas Gandriau <Nicolas.Gandriau@dealer.com>
2015-03-26 11:19:50 -0400

    deleted unrelevant stuff added .gitignore

--- src/main/groovy/switcher/WackyController.groovy
+++ src/main/groovy/switcher/WackyController.groovy
@@ -1,15 +1,12 @@
 package switcher
 
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 import javax.validation.Valid
-import java.util.concurrent.atomic.AtomicLong
 
-import static java.lang.String.format
 import static org.springframework.http.HttpStatus.NON_AUTHORITATIVE_INFORMATION
 import static org.springframework.web.bind.annotation.RequestMethod.POST
 
@@ -17,18 +14,6 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST
 @RequestMapping("/wacky")
 public class WackyController {
 
-    private static final String template = "Hello, %s!"
-    private static final AtomicLong counter = new AtomicLong()
-
-    @RequestMapping
-    public Greeting greeting(
-            @RequestParam(value = "name", defaultValue = "World") String name) {
-        Greeting greet = new Greeting()
-
-
-        return new Greeting(id: counter.incrementAndGet(), content: format(template, name),
-                fields: [key1:"val1", key2: "val2"])
-    }
 
     @RequestMapping(method = POST)
     @ResponseStatus(NON_AUTHORITATIVE_INFORMATION)
