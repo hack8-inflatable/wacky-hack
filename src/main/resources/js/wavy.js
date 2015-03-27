98f567748c69397d6e32ab46f99f8a83cd440a59
Ryan Gardner <ryan.gardner@dealer.com>
2015-03-27 13:02:43 -0400

    make the enable button do something

--- src/main/resources/js/wavy.js
+++ src/main/resources/js/wavy.js
@@ -9,17 +9,31 @@ app.service("wackyService", ["$http", function ($http) {
             });
     };
 
-    return {isWacky: isWacky};
+    function makeWacky(wackiness) {
+        $http({
+            method: "post",
+            url: "/wacky/switch",
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            params: { 'on': wackiness }
+        }).success(function () {});
+    }
+
+    return {isWacky: isWacky, makeWacky : makeWacky};
 }]);
 
 
 app.controller('wacky-controller', ["$scope", "wackyService", "$timeout", function ($scope, wackyService, $timeout) {
     function updateWackiness() {
         wackyService.isWacky().then(function (data) {
-            console.log(data);
             $scope.isWacky = data;
         });
     }
+
+    $scope.toggleWackiness = function() {
+        wackyService.makeWacky(!$scope.isWacky);
+        updateWackiness();
+    };
+
     var poll = function() {
             $timeout(function() {
                 updateWackiness();
